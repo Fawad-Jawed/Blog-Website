5e4299cedc9683c16f35051936e1157a77ab6187
Fawad Jawed <154456257+Fawad-Jawed@users.noreply.github.com>
2024-08-19 01:55:33 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
-// import { renderBlogs } from "./dashboard.js";
-// import { collection, query, orderBy, onSnapshot } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
-// import { db } from './firebase.mjs';
-
-// const blogsContainer = document.getElementById('blogsContainer');
-
-// const q = query(collection(db, 'blogs'), orderBy('date', 'desc'));
-// onSnapshot(q, (querySnapshot) => {
-//     const blogs = querySnapshot.docs.map(doc => ({
-//         id: doc.id,
-//         ...doc.data()
-//     }));
-//     renderBlogs(blogs);
-// });
+// import {renderBlogs} from "./dashboard.js"
+// renderBlogs()
 
 const heading = document.getElementById("head");
 const currentHour = new Date().getHours();
@@ -21,28 +9,3 @@ if (currentHour < 12) {
 } else {
     heading.innerText = "Good Afternoon Readers!";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import {renderBlogs} from "./dashboard.js"
-// renderBlogs()
-// const heading = document.getElementById("head");
-// const currentHour = new Date().getHours();
-
-// if (currentHour < 12) {
-//   heading.innerText = "Good Morning Readers!";
-// } else {
-//   heading.innerText = "Good Afternoon Readers!";
-// }
