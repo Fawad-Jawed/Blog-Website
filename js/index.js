624b75462eae7b41bd13017076f2d1af5c2f1597
Fawad Jawed <154456257+Fawad-Jawed@users.noreply.github.com>
2024-08-19 01:29:13 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,44 @@
-import {renderBlogs} from "./dashboard.js"
-renderBlogs()
-const heading = document.getElementById("head");
-const currentHour = new Date().getHours();
+document.getElementById('newBlogForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-if (currentHour < 12) {
-  heading.innerText = "Good Morning Readers!";
-} else {
-  heading.innerText = "Good Afternoon Readers!";
-}
+    const title = document.getElementById('title').value;
+    const body = document.getElementById('body').value;
+
+    try {
+        await addDoc(collection(db, 'blogs'), {
+            title,
+            body,
+            date: new Date(),
+            userId: auth.currentUser.uid
+        });
+        document.getElementById('newBlogForm').reset();
+        // Redirect to index.html
+        window.location.href = "index.html";
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {renderBlogs} from "./dashboard.js"
+// renderBlogs()
+// const heading = document.getElementById("head");
+// const currentHour = new Date().getHours();
+
+// if (currentHour < 12) {
+//   heading.innerText = "Good Morning Readers!";
+// } else {
+//   heading.innerText = "Good Afternoon Readers!";
+// }
