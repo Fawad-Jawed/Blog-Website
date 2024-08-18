0367397fcd3ad332dd1174870afb7a69e71ca5f1
Fawad Jawed <154456257+Fawad-Jawed@users.noreply.github.com>
2024-08-19 00:22:07 +0500

    Update signIn.js

--- js/signIn.js
+++ js/signIn.js
@@ -2,16 +2,21 @@ import { auth } from "./firebase.mjs";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 const btn2 = document.getElementById("btn");
 
-btn2.addEventListener("click",async (e) => {
-        e.preventDefault();
+btn2.addEventListener("click", () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("pass").value;
 
-    try {
-        await signInWithEmailAndPassword(auth, email, password);
-            alert("Sign-In Successfully");
-        window.location.href = 'dashboard.html';
-    } catch (error) {
-        console.error("Error signing in:", error);
-    }
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("user ==== ", user);
+      alert("Sign-In Successfully")
+        window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("error code ===", errorCode);
+      console.log("error message === ", errorMessage);
+    });
 });
